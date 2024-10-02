4a6b42a09a0cf4b91092a8c67bed456bcd268370
Joe-Pacifiker <145924934+Joe-Pacifiker@users.noreply.github.com>
2024-10-02 16:01:05 -0700

    Add starting states and initialize output regs
    Few other things added, will further document this weekend

--- microSD/src/microSD.v
+++ microSD/src/microSD.v
@@ -12,6 +12,7 @@ module microSD(
 
 
  wire [47:0] sdio_cmd_in;
+ reg [5:0] cmd_bit_counter = 48;
 
  reg start_bit = 1'b0; // From FPGA to microSD card target
  reg transmission_bit = 1'b1; // Direction '0' card -> host '1' host -> card
@@ -20,11 +21,44 @@ module microSD(
  reg [6:0] crc7 = 7'b0000000; // Does this matter???
  reg end_bit = 1'b1; // Always 1 for host to card
 
+ reg sdio_cmd_en = 1'b1; // Remember enable line for tristate buffer!
+
+ //Initialize all output registers:
+ sdio_clk = 1'b1;
+ sdio_cmd = 1'b0;
+ sdio_d0 = 1'b0;
+ sdio_d1 = 1'b0;
+ sdio_d2 = 1'b0;
+ sdio_d3 = 1'b0;
+
  // Design an SDIO FSM to drive the cmd/response system
+ // Starting states:
+ // - SET_CMD_BIT: sets next sdio command bit to sdio_cmd output register
+ // - SEND_CMD_BIT: provides rising edge of sdio_clk to send the command bit
+ //                 decrements the sdio command bit counter
+ //                 if the bit counter was 1, transition to wait state to poll response
+ //                   also lower the sdio command enable line
+ // - WAIT_RESPONSE: Continues to check sdio command line for the start bit
+ //                  Once the start bit is found store it in a register... TBD
+
+ parameter SET_CMD_BIT = 0;
+ parameter SEND_CMD_BIT = 1;
+ parameter WAIT_RESPONSE = 2;
+
+ reg [31:0] sdio_state = SET_CMD_BIT;
 
  always@(posedge clk)
  begin
- 
+  case(sdio_state)
+   SET_CMD_BIT:
+   begin
+    if(sdio_clk)
+    begin
+     
+    end
+   end
+  endcase
+  sdio_clk <= ~sdio_clk; //toggle sdio_clk each rising edge of clk
  end
 
 
